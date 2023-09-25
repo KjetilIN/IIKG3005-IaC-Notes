c169abc254056061b81dd43c86005e37164bbac7
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-09-25 14:19:52 +0200

    module-4: test plan

--- module-4/module-demo/main.tf
+++ module-4/module-demo/main.tf
@@ -1,31 +1,31 @@
 terraform {
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = "3.69.0"
+      source  = "hashicorp/azurerm"
+      version = "3.69.0"
     }
   }
 }
 
 provider "azurerm" {
-    features {
-      
-    }
-  
+  features {
+
+  }
+
 }
 
 
 # Defining a module 
 module "ResouceGroup" {
-  source = "./ResourceGroup"
+  source    = "./ResourceGroup"
   base_name = "tf-demo"
-  location = "westeurope"
-  
+  location  = "westeurope"
+
 }
 
 module "StorageAccount" {
-  source = "./StorageAccount"
+  source    = "./StorageAccount"
   base_name = "TFdemo"
-  rgname = module.ResouceGroup.rg_name_output
-  location = "westeurope"
+  rgname    = module.ResouceGroup.rg_name_output
+  location  = "westeurope"
 }
