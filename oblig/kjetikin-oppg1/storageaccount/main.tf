303431dd2a132ac2a9f1c18537c1f33b2da8c2b0
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-09-26 13:42:18 +0200

    oblig-1: add storage account

---
+++ oblig/kjetikin-oppg1/storageaccount/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
+
+# For creating random ID with 3 characters 
+resource "random_string" "random_string" {
+  length  = 3
+  special = false
+  upper   = false
+}
+
+# Resource group for the storage account 
+resource "azurerm_resource_group" "rg_st" {
+  name     = var.rg_st_name
+  location = var.rg_st_location
+}
+
+# Storage account
+resource "azurerm_storage_account" "st" {
+  name                     = format("st",lower(var.project_name), random_string.random_string.result)
+  resource_group_name      = azurerm_resource_group.rg_st.name
+  location                 = azurerm_resource_group.rg_st.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+}
+
+# Storage container for the storage account 
+resource "azurerm_storage_container" "sc" {
+  name                  = format("sc",lower(var.project_name), random_string.random_string.result)
+  storage_account_name  = azurerm_storage_account.st.name
+  container_access_type = "private"
+}
---
+++ oblig/kjetikin-oppg1/storageaccount/variables.tf
@@ -0,0 +1,14 @@
+variable "project_name" {
+  type = string
+  description = "Name of the project"
+}
+
+variable "rg_st_name" {
+    type = string
+    description = "Name of the resource group for the storage account"
+}
+
+variable "rg_st_location" {
+  type = string
+  description = "Location of resource group for the storage account"
+}
