b41f1bd1b480628307ab97faa7a3ded4c97e39be
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-10-07 19:15:26 +0200

    Week39: variables have default variables

--- module-6/web-demo/variables.tf
+++ module-6/web-demo/variables.tf
@@ -1,26 +1,30 @@
 variable "location" {
     type = string
     description = "Location of the resource"
+    default = "westeurope"
 }
 
 variable "rg_name" {
     type = string
     description = "Name of the resource group"
+    default = "myrg"
 }
 
 variable "sa_name" {
     type = string
     description = "Storage account name"
+    default = "saname"
 }
 
 variable "source_content" {
     type = string
     description = "Content of the website"
+    default     = "<h1>Made with Terraform - CI/CD</h1>"
+
 }
 
 variable "index_document" {
     type = string
     description = "Name of the index html document"
     default = "index.html"
-  
 }
