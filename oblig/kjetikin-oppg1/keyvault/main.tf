815a2ee2f2030509788210ebccb117b162c71fd5
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-10-08 21:00:29 +0200

    fix: linting

--- oblig/kjetikin-oppg1/keyvault/main.tf
+++ oblig/kjetikin-oppg1/keyvault/main.tf
@@ -54,15 +54,15 @@ resource "azurerm_key_vault" "kv" {
     object_id = data.azurerm_client_config.current.object_id
 
     key_permissions = [
-      "Get", "Set", "List", "Delete"
+      "Get", "Set", "List", "Delete",
     ]
 
     secret_permissions = [
-      "Get", "Set", "List", "Delete"
+      "Get", "Set", "List", "Delete",
     ]
 
     storage_permissions = [
-      "Get", "Set", "List", "Delete"
+      "Get", "Set", "List", "Delete",
     ]
   }
   # Tags
