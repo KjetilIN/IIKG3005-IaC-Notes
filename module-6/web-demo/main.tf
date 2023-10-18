8fa40f875bca2f2224c3a3a187b9584b99f8747e
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-10-18 17:12:50 +0200

    test: nr 8

--- module-6/web-demo/main.tf
+++ module-6/web-demo/main.tf
@@ -32,6 +32,7 @@ resource "azurerm_storage_account" "sa_web" {
 }
 
 # Add a index.html file to the storage account
+# The content is simple for this website
 resource "azurerm_storage_blob" "index_html" {
   name                   = var.index_document
   storage_account_name   = azurerm_storage_account.sa_web.name
