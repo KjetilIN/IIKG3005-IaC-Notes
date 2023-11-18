ab2611cb87dfe589dd55135aac8a3bf92b53c724
Kjetil Indrehus <kjetilindrehus@gmail.com>
2023-11-18 21:00:44 +0100

    fix: correctly configured the providers

--- oblig/kjetikin-oppg1-cpy/keyvault/provider.tf
+++ oblig/kjetikin-oppg1-cpy/keyvault/provider.tf
@@ -1,5 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.69.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {
+    # Key vault features 
     key_vault {
       purge_soft_delete_on_destroy    = true
       recover_soft_deleted_key_vaults = true
